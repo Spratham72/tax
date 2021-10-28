8bc15717a092e605aaf7c37d9e81a118146c3082
Spratham72 <40023291+Spratham72@users.noreply.github.com>
2021-10-29 01:09:38 +0530

    Update toEqual.test.js

--- toEqual.test.js
+++ toEqual.test.js
@@ -1,6 +1,24 @@
 test("true",()=>{
     expect( {} ).toEqual( {} )
 })
+test("true",()=>{
+    expect( [] ).toEqual( [] )
+})
+test("true",()=>{
+    expect( { a: 1, b: 2 } ).toEqual( { a:1 , b:2 } )
+})
+test("true",()=>{
+   expect( [ 1, 2, 3 ] ).toEqual( [ 1, 2, 3 ] )
+})
+test("true",()=>{
+    expect( { a: 1, b: { c: 1, d: 2 }} ).toEqual( { a: 1, b: { c: 1, d: 2 }} ) 
+})
+test("true",()=>{
+    expect( [ 1, 2, [ 1, 2 ] ] ).toEqual( [ 1, 2, [ 1, 2 ]  ] )
+})
 test("false",()=>{
-    expect( [ 1, 2, [ 1, 3] ] ).not.toEqual( [ 1, 2, [ 1, 2]  ] )
+    expect( { a: 1, b: { c: 1, d: 2 }} ).toEqual( { a: 1, b: { e: 1, d: 2 }} )
+})
+test("false",()=>{
+    expect( [ 1, 2, [ 1, 3] ] ).toEqual( [ 1, 2, [ 1, 2]  ] )
 })
